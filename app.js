751dd530f903ba68e1e816d2aabeaa7fc5604c3b
junayet <muhammadjunayetmaruf@gmail.com>
2024-10-01 07:41:34 +0300

    backend has setup

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const expressMongoSanitize = require('express-mongo-sanitize');
+const cors = require('cors');
+const hpp = require('hpp');
+const expressRateLimit = require('express-rate-limit');
+
+// express-mongo-sanitize use to user-supplied data to prevent MongoDB Operator Injection.
+app.use(expressMongoSanitize());
+
+// Using cors enable CORS with various options.
+
+app.use(cors());
+
+// hpp protect against HTTP Parameter Pollution attacks
+
+app.use(hpp());
+
+//Use to limit repeated requests to public APIs and/or endpoints such as password reset.
+
+const limiter = expressRateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+})
+
+// Apply the rate limiting middleware to all requests.
+app.use(limiter)
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({limit: '50mb'}));
+
+
+const publicRoute = require("./src/routes/publicRoutes");
+// const privateRoute = require("./src/routes/privateRoute");
+
+app.get("/", (req, res) => {
+    res.send("Server is healthy");
+})
+
+//  Public routes
+
+app.use("/api/v1",publicRoute);
+
+// Private Routes
+
+// app.use("/api/v1",privateRoute);
+
+
+
+
+module.exports = app;
---
+++ db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+require("dotenv").config();
+
+const connectDb = () =>{
+    try {
+        const dbPort = process.env.DB_URI;
+        
+        mongoose.connect(dbPort);
+        console.log(`MongoDB Connected Successfully`);
+    }catch (e) {
+        console.log(`MongoDB Connection Error: ${e}`);
+    }
+};
+
+
+module.exports = connectDb;
