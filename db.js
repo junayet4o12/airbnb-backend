a454931c64abc459e09b2f58cc46a400ad7d4f97
junayet <muhammadjunayetmaruf@gmail.com>
2024-10-04 18:31:23 +0300

    changes

--- db.js
+++ db.js
@@ -1,13 +1,32 @@
 const mongoose = require('mongoose');
-require("dotenv").config();
+require('dotenv').config();
+
 const uri = process.env.DB_URI;
+
+let isConnected = false; // Keep track of the connection status
+
 const connectDb = async () => {
-    console.log('connecting');
-    
-    await mongoose.connect(uri, { dbName: 'airbnb' })
-    console.log('connected')
-}
+  if (isConnected) {
+    console.log('=> using existing database connection');
+    return;
+  }
+
+  try {
+    console.log('=> connecting to database');
+
+    await mongoose.connect(uri, {
+      dbName: 'airbnb',
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000, // Timeout after 5s if unable to connect
+    });
+
+    isConnected = mongoose.connection.readyState === 1; // Set the status to connected
+    console.log('=> connected to database');
+  } catch (error) {
+    console.error('Error connecting to the database', error);
+    throw new Error('Database connection failed'); // Throw error to handle it in the calling function
+  }
+};
+
 module.exports = connectDb;
-
-
-
