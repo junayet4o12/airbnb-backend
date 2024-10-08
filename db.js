8f1716ffb6777dc1357dee5cc272e0305d7bac06
junayet <muhammadjunayetmaruf@gmail.com>
2024-10-08 10:34:29 +0300

    filtering has updated

--- db.js
+++ db.js
@@ -1,32 +1,13 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-
 const uri = process.env.DB_URI;
-
-let isConnected = false; // Keep track of the connection status
-
 const connectDb = async () => {
-  if (isConnected) {
-    console.log('=> using existing database connection');
-    return;
-  }
-
-  try {
-    console.log('=> connecting to database');
-
-    await mongoose.connect(uri, {
-      dbName: 'airbnb',
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 5000, // Timeout after 5s if unable to connect
-    });
-
-    isConnected = mongoose.connection.readyState === 1; // Set the status to connected
-    console.log('=> connected to database');
-  } catch (error) {
-    console.error('Error connecting to the database', error);
-    throw new Error('Database connection failed'); // Throw error to handle it in the calling function
-  }
-};
-
+    console.log('connecting');
+    
+    await mongoose.connect(uri, { dbName: 'airbnb' })
+    console.log('connected')
+}
 module.exports = connectDb;
+
+
+
--- src/controllers/propertyControllers.js
+++ src/controllers/propertyControllers.js
@@ -20,10 +20,12 @@ const getAllProperties = async (req, res) => {
 
 const filterProperty = async (req, res) => {
   try {
-    const { location, checkIn, checkout, category, adults, children, infants, pets } = req.body;
-    console.log(req.body);
-
-
+    const { location, checkIn, checkout, category, adults, children, infants, pets, price, type, roomsAndBeds, amenities } = req.body;
+    const { room, bed, bathroom } = roomsAndBeds;
+    const { wifi, kitchen, washer } = amenities;
+    let { min = 0, max = 550 } = price;
+    min = min === null ? 0 : min;
+    max = max === null ? 550 : max;
     const query = {};
 
 
@@ -41,8 +43,20 @@ const filterProperty = async (req, res) => {
       if (infants) query['acceptableGuestAmount.infants'] = { $gte: infants };
       if (pets) query['acceptableGuestAmount.pets'] = { $gte: pets };
     }
+    query['roomsAndBeds.rooms'] = { $gte: room };
+    query['roomsAndBeds.beds'] = { $gte: bed };
+    query['roomsAndBeds.bathrooms'] = { $gte: bathroom };
 
+    if (wifi || kitchen || washer) {
+      if (wifi) query['amenities.wifi'] = true;
+      if (kitchen) query['amenities.kitchen'] = true;
+      if (washer) query['amenities.washer'] = true;
 
+    }
+    if (type && type !== 'anyType') {
+      query.typeOfPlace = type;
+    }
+    query.pricePerNight = { $gte: min, $lte: max };
     let properties = await Property.find(query);
 
     if (checkIn) {
